860db64dd7ce81f2b07104f650297dd87a568433
omnia <omniabashandy@gmail.com>
2024-11-03 13:06:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Routing from "./components/Routing";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
-    <div className='container py-2'>
-      <Header />
-      <Nav />
-    </div>
-      <Main />
+      <Navbar />
+      <Routing />
       <Footer />
     </>
   );
