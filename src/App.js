2a358514b8d504d0da70974b8cd58d309a9d6567
omnia <omniabashandy@gmail.com>
2024-11-03 13:06:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
-    <div className='container'>
+    <div className='container py-2'>
       <Header />
       <Nav />
     </div>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-    <div>
-      <h1>Hero Section</h1>
+    <div className='bg-495E57 p-0 m-0'>
+      <h1 className='f-F4CE14'>Little Lemon</h1>
     </div>
   )
 }
